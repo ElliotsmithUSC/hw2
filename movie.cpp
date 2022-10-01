80170820a27e66cf58ef0a5bb3705a1d49b58315
Elliot Smith <elliotas@usc.edu>
2022-10-01 14:31:09 -0700

    final files

--- movie.cpp
+++ movie.cpp
@@ -6,10 +6,10 @@
 
 using namespace std;
 
-Movie::Movie(const std::string name, double price, int qty, const std::string genre, const std::string rating){
-Product("movie", name, price, qty),
-genre_(genre),
-rating_(rating),
+Movie::Movie(const std::string name, double price, int qty, const std::string genre, const std::string rating):
+Product("movie", name, price, qty){
+genre_=genre;
+rating_=rating;
 }
 
 
@@ -17,24 +17,35 @@ Movie::~Movie(){
 
 }
 
-std::set<std::string> Movie::keywords() const{
+std::set<std::string> Movie::keywords() const{//stitches all searchable keywords into one set
 	set<string> keyword;
-	set<string> tempkey;
+	string tempkey;
 	string valget = getName();
 	keyword = parseStringToWords(valget);
 	tempkey = {genre_};
-	keyword = setUnion<string>(keyword, tempkey);
+	keyword.insert(tempkey);
 	return keyword;
 }
 
-std::string Movie::displayString() const{
-/*std::string returnval = std::string("Title: ") + name_ + (" Written by: ") + author_ + (" Price: ") + price_;
-return returnval;*/
-std::stringstream s;
-s << name_ << " Genre: " << genre_ << " $" << price_ << " " << qty_ << " copies remain";
-string returnstring;
-s >> returnstring;
-return returnstring;
+std::string Movie::displayString() const{//displays product in correct fashion
+	string returnstring(name_);
+	stringstream s1, s2, s3, s4;
+	s1 << price_;
+	s2 << genre_;
+	s3 << rating_;
+	s4 << qty_;
+	returnstring.append("\n");
+	returnstring.append("Genre:");
+	returnstring.append(s2.str());
+	returnstring.append(" Rating:");
+	returnstring.append(s3.str());
+	returnstring.append("\n");
+	returnstring.append("$");
+	returnstring.append(s1.str());
+	returnstring.append(" ");
+	returnstring.append(s4.str());
+	returnstring.append(" left.");
+	return returnstring;
 }
 
 void Movie::dump(std::ostream& os) const{
