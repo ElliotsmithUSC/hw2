de959bb26274a80f71ac06fb65aa23ab5d2d1ac3
Elliot Smith <elliotas@usc.edu>
2022-10-01 15:04:59 -0700

    final files

--- util.h
+++ util.h
@@ -1,6 +1,5 @@
 #ifndef UTIL_H
 #define UTIL_H
-
 #include <string>
 #include <iostream>
 #include <set>
@@ -17,17 +16,17 @@ std::set<T> intersectionset;
 int size1 = s1.size();
 int size2 = s2.size();
 typename std::set<T>::iterator it;
-if(size1>=size2){
+if(size1>=size2){//checks if an element from 1 is in 2 if it is than it adds to the intersection set runs if s1 is larger than s2
 	for(it = s1.begin(); it!=s1.end(); ++it){
-		if(s2.find(it)!=s2.end()){
-			intersectionset.insert(it);
+		if(s2.find(*it)!=s2.end()){
+			intersectionset.insert(*it);
 		}
 	}
 }
 else{
-	for( it = s2.begin(); it!=s2.end(); ++it){
+	for( it = s2.begin(); it!=s2.end(); ++it){//same as above function but works for if s2 is larger than s1.
 		if(s1.find(*it)!=s1.end()){
-			intersectionset.insert(it);
+			intersectionset.insert(*it);
 		}
 	}
 }
@@ -39,19 +38,19 @@ return intersectionset;
 
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
-{
- std::set<T> unionset = s1;
+{ 
+ std::set<T> unionset = s1;//sets unionset to first input
  for(typename std::set<T>::iterator it = s2.begin(); it!=s2.end(); ++it){
-	 if(s1.find(*it)==s1.end()){
-		 unionset.insert(*it);
-	 }
- }
+		 unionset.insert(*it);//adds anything from second input to unionset that's not already in there
+ } 
 
 return unionset;
 
 
 
 }
+
+
 
 /***********************************************/
 /* Prototypes of functions defined in util.cpp */
